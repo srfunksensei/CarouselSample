d5727095a677b61808d702d6c6952a50dc23fde3
MB <srfunksensei@gmail.com>
2019-02-01 15:10:46 +0100

    carousel item swift coresponding file added

---
+++ CarouselSample/CarouselSample/item/CarouselItem.swift
@@ -0,0 +1,36 @@
+import Foundation
+import UIKit
+
+@IBDesignable
+class CarouselItem: UIView {
+    static let CAROUSEL_ITEM_NIB = "CarouselItem"
+    
+    @IBOutlet var vwContent: UIView!
+    @IBOutlet var vwBackground: UIView!
+    @IBOutlet var lblTitle: UILabel!
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initWithNib()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initWithNib()
+    }
+    
+    convenience init(titleText: String? = "", background: UIColor? = .red) {
+        self.init()
+        lblTitle.text = titleText
+        vwBackground.backgroundColor = background
+    }
+    
+    fileprivate func initWithNib() {
+        Bundle.main.loadNibNamed(CarouselItem.CAROUSEL_ITEM_NIB, owner: self, options: nil)
+        vwContent.frame = bounds
+        vwContent.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        addSubview(vwContent)
+    }
+}
